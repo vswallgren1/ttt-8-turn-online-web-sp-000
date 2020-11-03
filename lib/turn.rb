569f406edb93c37ac23386e938bdeabc9fe79694
Vince W <vincewallgren@me.com>
2020-11-03 04:24:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ def valid_move?(board, position)
   if position.to_i.between?(1,9)
     if !position_taken?(board, position.to_i-1)
       return true
-    elsif
+    else
       return false
     end
   end
