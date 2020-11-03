302bcd39c48c3d0eeb00546ddb095ada2d7aab03
Vince W <vincewallgren@me.com>
2020-11-03 04:44:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ end
 
 def valid_move?(board, position)
   if position.to_i.between?(1,9)
-    if !position_taken?(board, position.to_i-1)
+    if position_taken?(board, position.to_i-1)
       return true
     elsif board[index] == "X" || "O"
       return false
