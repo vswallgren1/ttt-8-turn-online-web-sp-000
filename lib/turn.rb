fd346c1e565a1b5a2628c26ad8652ba8084aac42
Vince W <vincewallgren@me.com>
2020-11-03 05:12:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,21 +1,17 @@
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(number)
+  number.to_i - 1
+end
+
+def move(board, index, token='X')
+  board[index] = token
 end
 
 def position_taken?(board, index)
@@ -32,10 +28,14 @@ def valid_move?(board, index)
   end
 end
 
-def input_to_index(number)
-  number.to_i - 1
-end
-
-def move(board, index, token='X')
-  board[index] = token
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
